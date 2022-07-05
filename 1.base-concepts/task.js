8da3aa6f9b02e73e443fd732a23a41c4e3a7db18
Nataliya-grish <gray-wings@yandex.ru>
2022-07-05 23:14:09 +0300

    homeworks change

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,50 @@
+"use strict";
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+  let arr = [];
+  let d = Math.pow(b, 2) - 4 * a * c;
+  if (d < 0) {
+    return arr = [];
+  } else
+    if (d === 0) {
+      arr.push(-b / (2 * a));
+      return arr;
+    } else {
+      arr.push((-b + Math.sqrt(d)) / (2 * a));
+      arr.push((-b - Math.sqrt(d)) / (2 * a));
+
+
+      return arr;
+    }
 }
 
 function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+  let totalAmount
 
-  // код для задачи №2 писать здесь
+  let percentTest = Number(percent);
+  let contributionTest = Number(contribution);
+  let amountTest = Number(amount);
+  if (percentTest != Number(percent)) {
+    totalAmount = `Параметр "Процентная ставка" содержит неправильное значение "${percent}"`;
+    return totalAmount
+  } else if (contributionTest != Number(contribution)) {
+    totalAmount = `Параметр "Начальный взнос" содержит неправильное значение "${contribution}"`;
+    return totalAmount
+  } else if (amountTest != Number(amount)) {
+    totalAmount = `Параметр "Общая стоимость" содержит неправильное значение "${amount}"`;
+    return totalAmount
+  }
+  else {
+    let credit = amount - contribution;
+    let dateMonth = date.getMonth();
+    let dateYear = date.getFullYear();
+    let dateNow = new Date(Date.now());
+    let dateNowMonth = dateNow.getMonth();
+    let dateNowYear = dateNow.getFullYear();
+    let month = (dateYear - dateNowYear) * 12 + (dateMonth - dateNowMonth);
 
-  return totalAmount;
+    let payment = credit * (percent / 12 / 100 + (percent / 12 / 100 / (((1 + percent / 12 / 100) ** month - 1))));
+    totalAmount = payment * month;
+
+    return Number(totalAmount.toFixed(2));
+  }
 }
