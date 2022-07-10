914bebd51bf6b54e410791be12359c092b420370
Nataliya-grish <gray-wings@yandex.ru>
2022-07-10 16:36:20 +0300

    done homework

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,12 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+ let result = (arr1.length === arr2.length) && arr1.every((element, index) => element === arr2[index]);   
+    
+ return result; // boolean
 }
 
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
+    
+  let resultArr = arr.filter(namber => namber >= 0).filter(namber => namber % 3 == 0).map(namber => namber * 10)
 
   return resultArr; // array
 }
