95be19d6d59d4e3abaa80a8ecde3d4a0b4ee1413
Nataliya-grish <gray-wings@yandex.ru>
2022-07-24 14:50:24 +0300

    done homework

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,52 @@
+
 function Student(name, gender, age) {
-    // Ваш код
+    
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+
 
 }
+let student1 = new Student("Tony", "male", 37);
+let student2 = new Student("Buzz", "female", 35);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+   this.subject = subjectName;
 }
 
-// ваш код для остальных методов
+Student.prototype.addMark = function(mark) {
+ if(this.marks === undefined){ 
+    this.marks = [mark]; 
+    } else {
+      this.marks.push(mark);
+    }
+}
+
+Student.prototype.addMarks = function (...marks) {
+	if(this.marks === undefined){ 
+    this.marks = marks;
+    } else {
+    	for (let i = 0; i < marks.length; i++) {
+    		this.marks.push(marks[i]);
+    	}
+    }
+}
+
+Student.prototype.getAverage = function() {
+ return this.marks.reduce((acc, x) => acc + x)/this.marks.length;
+    
+}
+
+Student.prototype.exclude = function(reason) {
+  delete this.marks;
+  delete this.subject;
+  this.excluded = reason;
+}
+
+
+
+    
+    
+    
+    
+    
